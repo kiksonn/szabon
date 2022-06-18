84f15e0bde560e12183803859e6c7e9340a37cc9
Oskar Malinowski <kikson.yy@gmail.com>
2022-06-18 14:22:17 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+const nav = document.querySelector('.nav__list')
+const navBtn = document.querySelector('.hamburger')
+const allNavItems = document.querySelectorAll('.nav__list-item')
+// const footerYear = document.querySelector('.footer__year')
+
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active');
+    nav.classList.toggle('nav__list--active');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav__list--active')
+            navBtn.classList.remove('is-active')
+        })
+    }) 
+}
+
+// const handleCurrentYear = () => {
+//     const year = (new Date).getFullYear();
+//     footerYear.innerText = year;
+// }
+
+// handleCurrentYear();
+navBtn.addEventListener('click', handleNav)
+
