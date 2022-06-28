5d8ce1667df4b7874611faf94a542aa4c26fb7fd
Oskar Malinowski <kikson.yy@gmail.com>
2022-06-28 21:00:12 +0200

    little uprage js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const nav = document.querySelector('.nav__list')
 const navBtn = document.querySelector('.hamburger')
 const allNavItems = document.querySelectorAll('.nav__list-item')
+const teamTiles = document.querySelectorAll('.team__wrapper-tile')
 const footerYear = document.querySelector('.footer__year')
-
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
@@ -16,6 +16,31 @@ const handleNav = () => {
     })
 }
 
+function handleTeam() {
+    if (this.firstElementChild.classList.contains('active-team')) {
+        this.firstElementChild.classList.remove('active-team')
+    } else {
+        closehandleTeam()
+        this.firstElementChild.classList.toggle('active-team');
+    }
+
+}
+
+const closehandleTeam = () => {
+    const allActiveItems = document.querySelectorAll('.team__wrapper-tile-info')
+    allActiveItems.forEach(item => item.classList.remove('active-team'))
+}
+
+const clickOutsideTeam = e => {
+    if (e.target.classList.contains('team__wrapper-tile') || e.target.classList.contains('team__wrapper-tile-info') ||
+        e.target.classList.contains('team-title-three') || e.target.classList.contains('team-text')
+    ) {
+        return
+    } else {
+        closehandleTeam()
+    }
+}
+
 const handleCurrentYear = () => {
     const year = (new Date).getFullYear();
     footerYear.innerText = year;
@@ -23,4 +48,5 @@ const handleCurrentYear = () => {
 
 handleCurrentYear();
 navBtn.addEventListener('click', handleNav)
-
+teamTiles.forEach(teamTile => teamTile.addEventListener('click', handleTeam))
+window.addEventListener('click', clickOutsideTeam)
